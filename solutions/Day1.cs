2997b98d9d02e2106205396e14c20e6fd062f7eb
Tim Kraemer <tim.kraemer@cas.de>
2022-12-01 13:41:23 +0100

    Solution Day 1

--- solutions/Day1.cs
+++ solutions/Day1.cs
@@ -1,23 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace AdventOfCode2022.solutions
 {
     public class Day1 : ProjectDay
     {
+        
+        
         public override void Run()
         {
-            List<int> depthList = AOCUtility.FileLines(@"resources/day1/input.txt").Select(int.Parse).ToList();
+            var stopper = Stopwatch.StartNew();
+            
+            var sums = new List<int>();
+            var curSum = 0;
+            AOCUtility.FileLines(@"resources/day1/input.txt").ForEach(s =>
+            {
+                if (s == "")
+                {
+                    sums.Add(curSum);
+                    curSum = 0;
+                }
+                else
+                {
+                    curSum += Int32.Parse(s);
+                }
+            });
+            sums.Add(curSum);
 
-            int depthCounter1 = Enumerable.Range(0, depthList.Count - 1)
-                .Count(x => (depthList[x] < depthList[x + 1]));
-            int depthCounter2 = Enumerable.Range(0, depthList.Count - 3)
-                .Count(x => (depthList[x] < depthList[x + 3]));
-
-            Log($"{depthCounter1}", Main.LogLevel.Result1);
-
-            Log($"{depthCounter2}", Main.LogLevel.Result2);
+            Log($"{sums.Max()}", Main.LogLevel.Result1);
+            
+            var orderedList = sums.OrderBy(i => i).Reverse().ToList();
+            
+            Log($"stopped at: {stopper.Elapsed}");
+            stopper.Restart();
+            
+            Log($"{orderedList[0] + orderedList[1] + orderedList[2]}", Main.LogLevel.Result2);
+            
+            Log($"stopped at: {stopper.Elapsed}");
     }
 }
 
